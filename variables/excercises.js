188b5ddcc6612ac5b0e74500be1b4ffb75b980f1
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2017-11-19 23:02:15 +0100

    Add files via upload

--- variables/excercises.js
+++ variables/excercises.js
@@ -1,16 +1,11 @@
 /*
-
 Gebruik van dit document
-
 De opgave in een Block Comment
-
 De code staat tussen Line Comments.
 Commentarieer deze uit en los de opgave op.
 Gebruik de Webconsole om de
 uitvoer te bekijken en voor de Warnings en Error meldingen.
-
 Je code is pas goed als er geen Warnings en Errors zijn!
-
 */
 
 
@@ -20,39 +15,35 @@ Je code is pas goed als er geen Warnings en Errors zijn!
     Opgave 1.
     De variabele is function scoped dus niet zichtbaar buiten de functie.
     Vind een manier om deze toch zichtbaar te maken
-
     LET OP
     Geen globale variabele width aanmaken
-
  */
 
-// function setWidth() {
-//     var width = 100;
-// }
-// console.log(width);
 
+// console.log(setWidth() + "lol");
+// function setWidth() {
+//     let width = 100;
+//     return width;
+// }
+// Hij console.logt niks bij mij
 /*
     Opgave 2.
     De variabele boxSize is 'leaking' naar de global scope.
-
     Los het 'leaking' probleem op.
-
  */
 
-// var size = 100;
-// if (size > 50) {
-//     var boxSize = size * 5;
-//     console.log(boxSize);
-// }
-// console.log(boxSize);
+var size = 100;
+if (size > 50) {
+    var boxSize = size * 5;
+    console.log(boxSize);
+}
+console.log(boxSize);
 
 /*
     Opgave 3.
     Een 'let' of 'const' variabele kan niet opnieuw worden aangemaakt.
-
     Verklaar waarom het in onderstaande voorbeeld de bonus variabele wel op true gezet kan worden.
     Zet je oplossing als commentaar in de code.
-
  */
 
 // let winningPoints = 100;
@@ -68,17 +59,14 @@ Je code is pas goed als er geen Warnings en Errors zijn!
     Maak een const object aan uit je directe omgeving met een 4-tal properties.
     Denk bijv. aan een object als klaslokaal, of een persoon.
     HINT: Een object maak je met behulp van {}
-
  */
 
 /*
     Opgave 5.
     De waarde van een const kan je niet wijzigen.
-
     Los het probleem in de onderstaande code op
     maar het object person moet een const blijven.
     De overlijdensdatum van Bowie is 2016.
-
  */
 
 // const person = {
@@ -97,13 +85,10 @@ Je code is pas goed als er geen Warnings en Errors zijn!
 /*
     Opgave 6.
     In de HTML staat een script dat het window een naam geeft.
-
     De onderstaande code overschrijft deze naam doordat de variabele
     in de global scope staat. Dat is dus de window.name variabele.
-
     Maak een IIFE die dit probleem voorkomt en tijdelijk dus de inhoud
     van deze les weergeeft.
-
  */
 
 // var name = 'Lesson 1: ES6 Variables';
@@ -114,7 +99,6 @@ Je code is pas goed als er geen Warnings en Errors zijn!
     Opgave 7.
     Herschrijf de code van opgave 6, maak gebruik van de ES6 syntax.
     OPM: Oh ja, de naam van de variabele mag je dus niet wijzigen.
-
  */
 
 
@@ -123,7 +107,6 @@ Je code is pas goed als er geen Warnings en Errors zijn!
     Onderstaande code is bijna gelijk aan de bovenstaande, voorkom dat de
     globale variabele overschreven wordt.
     OPM: Oh ja, de naam van de variabele mag je dus niet wijzigen.
-
  */
 
 // function varLeak () {
@@ -135,11 +118,8 @@ Je code is pas goed als er geen Warnings en Errors zijn!
 
 
 /*
-
     For Loop Sample.
     Gebruik van var in een for loop zet de variabele op de global scope.
-
-
  */
 
 // for (var i = 0; i < 3; i++) {
@@ -155,9 +135,8 @@ Je code is pas goed als er geen Warnings en Errors zijn!
  /*
     Temporal Dead Zone
     Lees documentatie over hoisting na
-
     Experimenteer met Const en Let, gebruik de console voor error checking
-    
+
   */
 
 //  function tdz () {
